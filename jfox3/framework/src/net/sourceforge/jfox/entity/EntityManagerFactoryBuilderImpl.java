25bc5fc0940d36149f5994092f5044a9e2de8026
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-03-08 07:23:56 +0000

    parse cache config in persistence.xml

--- jfox3/framework/src/net/sourceforge/jfox/entity/EntityManagerFactoryBuilderImpl.java
+++ jfox3/framework/src/net/sourceforge/jfox/entity/EntityManagerFactoryBuilderImpl.java
@@ -335,7 +335,6 @@ public class EntityManagerFactoryBuilderImpl implements EntityManagerFactoryBuil
                 else {
                     logger.warn("Illegal JPA cache property name: " + name);
                 }
-
             }
             else {
                 logger.warn("Illegal JPA persistence.xml property name: " + name);
