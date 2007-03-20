9abf1e97684c4869d042ca1beddefec6050679cd
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-03-20 16:02:35 +0000

    FreemarkerRender added! @Helper removed!

--- jfox3/MODULES/manager/src/net/sourceforge/jfox/manager/CartsAction.java
+++ jfox3/MODULES/manager/src/net/sourceforge/jfox/manager/CartsAction.java
@@ -1,9 +1,14 @@
 package net.sourceforge.jfox.manager;
 
-import net.sourceforge.jfox.mvc.ActionSupport;
-import net.sourceforge.jfox.mvc.InvocationContext;
-import net.sourceforge.jfox.mvc.annotation.ActionMethod;
+import java.util.ArrayList;
+
 import net.sourceforge.jfox.framework.annotation.Service;
+import net.sourceforge.jfox.mvc.ActionSupport;
+import net.sourceforge.jfox.mvc.Invocation;
+import net.sourceforge.jfox.mvc.InvocationContext;
+import net.sourceforge.jfox.mvc.SessionContext;
+import net.sourceforge.jfox.mvc.PageContext;
+import net.sourceforge.jfox.mvc.annotation.ActionMethod;
 
 /**
  * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
@@ -15,7 +20,48 @@ public class CartsAction extends ActionSupport {
     public void doGetView(InvocationContext invocationContext) throws Exception{
 
     }
-    
+
+    @ActionMethod(successView = "template/carts.fhtml", invocationClass = CartInvocation.class)
+    public void doPostSubmit(InvocationContext invocationContext) throws Exception {
+        CartInvocation invocation = (CartInvocation)invocationContext.getInvocation();
+        SessionContext sessionContext = invocationContext.getSessionContext();
+        ArrayList<String> carts = (ArrayList<String>)sessionContext.getAttribute("carts");
+        if(carts == null) {
+            carts = new ArrayList<String>();
+            sessionContext.setAttribute("carts", carts);
+        }
+
+        if(invocation.getSubmit().equals("add")) {
+            carts.add(invocation.getItem());
+        }
+        else if(invocation.getSubmit().equals("remove")){
+            carts.remove(invocation.getItem());
+        }
+        PageContext pageContext = invocationContext.getPageContext();
+        pageContext.setAttribute("carts", carts);
+    }
+
+    public static class CartInvocation extends Invocation {
+        private String item;
+        private String submit;
+
+        public String getItem() {
+            return item;
+        }
+
+        public void setItem(String item) {
+            this.item = item;
+        }
+
+        public String getSubmit() {
+            return submit;
+        }
+
+        public void setSubmit(String submit) {
+            this.submit = submit;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
