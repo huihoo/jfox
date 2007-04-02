895f3db5d43ad1370c853b7f32e98aa2dfce9edc
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-02 07:46:54 +0000

    SecurityContext improved!

--- jfox3/framework/src/org/jfox/ejb3/interceptor/InterceptorMethod.java
+++ jfox3/framework/src/org/jfox/ejb3/interceptor/InterceptorMethod.java
@@ -3,6 +3,8 @@ package org.jfox.ejb3.interceptor;
 import javax.interceptor.InvocationContext;
 
 /**
+ * Bean 中的 @Interceptors @InvokeAround 描述的方法
+ * 
  * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
  */
 public interface InterceptorMethod {
