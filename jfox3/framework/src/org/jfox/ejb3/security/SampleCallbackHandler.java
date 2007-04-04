d68f0eceb91059ace754610b2f83e8f98561e31a
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-04 09:30:13 +0000

    SecurityContext improved!

--- jfox3/framework/src/org/jfox/ejb3/security/SampleCallbackHandler.java
+++ jfox3/framework/src/org/jfox/ejb3/security/SampleCallbackHandler.java
@@ -6,16 +6,12 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
-import org.jfox.framework.component.Component;
-import org.jfox.framework.annotation.Service;
-
 /**
  * 根据 Callback的内容，实现登录
  *
  * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
  */
-@Service
-public class SampleCallbackHandler implements CallbackHandler, Component {
+public class SampleCallbackHandler implements CallbackHandler {
 
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
         JAASLoginRequestCallback requestLoginRequestCallback = (JAASLoginRequestCallback)callbacks[0];
