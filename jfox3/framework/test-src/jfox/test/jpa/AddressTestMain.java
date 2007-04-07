54f415e2c7c03481c3eb5e0a42519478bba11193
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-07 03:12:36 +0000

    test improved!

--- jfox3/framework/test-src/jfox/test/jpa/AddressTestMain.java
+++ jfox3/framework/test-src/jfox/test/jpa/AddressTestMain.java
@@ -10,10 +10,15 @@ import javax.persistence.EntityManagerFactory;
 public class AddressTestMain {
 
     public static void main(String[] args) {
+        // initialize EntityManager by persistence unit
         EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("default");
         final EntityManager em = emFactory.createEntityManager();
+
+        // query
         Address address = (Address)em.createNativeQuery("select * from address where ADR_ID = $id", Address.class).setParameter("id",1).getSingleResult();
         System.out.println("Address: " + address);
+
+        // close EntityManager, EntityManagerFactory
         em.close();
         emFactory.close();
     }
