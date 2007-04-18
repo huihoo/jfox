a23798eb86b7c9d47609eaefcbbee9d3400e93e7
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-18 00:50:35 +0000

    JMS

--- jfox3/framework/src/org/jfox/jms/destination/QueueMessagePool.java
+++ jfox3/framework/src/org/jfox/jms/destination/QueueMessagePool.java
@@ -1,5 +1,10 @@
 package org.jfox.jms.destination;
 
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.Comparator;
+import javax.jms.Message;
+import javax.jms.JMSException;
+
 /**
  * 由于只考虑支持本地 JMS，所以让 Destination 成为独立的 Container
  *
@@ -7,4 +12,16 @@ package org.jfox.jms.destination;
  */
 public class QueueMessagePool implements MessagePool{
 
+    PriorityBlockingQueue<Message> queue = new PriorityBlockingQueue<Message>(0, new Comparator<Message>(){
+
+        public int compare(Message o1, Message o2) {
+            try {
+                return Integer.valueOf(o1.getJMSPriority()).compareTo(Integer.valueOf(o1.getJMSPriority()));
+            }
+            catch(JMSException e) {
+                return 0;
+            }
+        }
+    });
+    
 }
