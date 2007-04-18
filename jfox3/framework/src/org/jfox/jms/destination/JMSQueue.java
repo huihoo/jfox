bc9975fd2ef01fd101ec40cdb3bf9dc590db0a89
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-18 07:59:21 +0000

    JMS

--- jfox3/framework/src/org/jfox/jms/destination/JMSQueue.java
+++ jfox3/framework/src/org/jfox/jms/destination/JMSQueue.java
@@ -12,10 +12,14 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Queue;
 import javax.jms.QueueReceiver;
+import javax.jms.MessageListener;
+
+import org.apache.log4j.Logger;
 
 /**
  * JMS Queue
@@ -23,17 +27,17 @@ import javax.jms.QueueReceiver;
  *
  * @author <a href="mailto:young_yy@hotmail.com">Young Yang</a>
  */
-public class JMSQueue extends JMSDestination implements Queue, Runnable{
+public class JMSQueue extends JMSDestination implements Queue, Runnable {
 
     private static ExecutorService threadExecutor = Executors.newCachedThreadPool();
 
-    private static Comparator<Message> MESSAGE_COMPARATOR = new Comparator<Message>(){
+    private static Comparator<Message> MESSAGE_COMPARATOR = new Comparator<Message>() {
 
         public int compare(Message msg1, Message msg2) {
             try {
                 return Integer.valueOf(msg1.getJMSPriority()).compareTo(Integer.valueOf(msg2.getJMSPriority()));
             }
-            catch(JMSException e) {
+            catch (JMSException e) {
                 return 0;
             }
         }
@@ -43,19 +47,23 @@ public class JMSQueue extends JMSDestination implements Queue, Runnable{
 
     private List<QueueReceiver> receivers = new ArrayList<QueueReceiver>(2);
 
+    private final ReentrantLock lock = new ReentrantLock();
+
+    Logger logger = Logger.getLogger(JMSQueue.class);
+
     public JMSQueue(String name) {
-		super(name);
-	}
+        super(name);
+    }
 
-	public String getQueueName() throws JMSException {
-		return getName();
-	}
+    public String getQueueName() throws JMSException {
+        return getName();
+    }
 
-	public boolean isTopic() {
-		return false;
-	}
+    public boolean isTopic() {
+        return false;
+    }
 
-    public synchronized void putMessage(Message msg){
+    public synchronized void putMessage(Message msg) {
         queue.offer(msg);
         // 交给线程池执行消息分发工作
         threadExecutor.submit(this);
@@ -65,26 +73,40 @@ public class JMSQueue extends JMSDestination implements Queue, Runnable{
         try {
             return queue.take();
         }
-        catch(InterruptedException e) {
+        catch (InterruptedException e) {
             //TODO: logger
             e.printStackTrace();
             return null;
         }
     }
 
-    public void stop(){
+    public void stop() {
         threadExecutor.shutdown();
     }
 
     public void run() {
-        // 分发消息
-        Message message = popMessage();
-        if(message != null) {
-            System.out.println("Hello,World!");
+        try {
+            lock.lock(); // 获得锁
+            // 分发消息
+            Message message = popMessage();
+            if (message != null) {
+                System.out.println("Hello,World!");
+                if(!receivers.isEmpty()) {
+                    QueueReceiver receiver = receivers.get(0);
+                    MessageListener messageListener = receiver.getMessageListener();
+                    messageListener.onMessage(message);
+                }
+            }
+        }
+        catch(JMSException e) {
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
         }
     }
-    
+
     public static void main(String[] args) {
 
-	}
+    }
 }
