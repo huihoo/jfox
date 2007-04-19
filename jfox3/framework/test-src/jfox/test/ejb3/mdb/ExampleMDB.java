fbf8958335d33ce8a65fed5a7c95c98d9cc16d19
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-19 00:59:21 +0000

    JMS

---
+++ jfox3/framework/test-src/jfox/test/ejb3/mdb/ExampleMDB.java
@@ -0,0 +1,24 @@
+package jfox.test.ejb3.mdb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.MessageListener;
+import javax.jms.Message;
+
+/**
+ * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
+ */
+@MessageDriven(activationConfig =
+        {
+        @ActivationConfigProperty(propertyName="destinationType", propertyValue="javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName="destination", propertyValue="queue/tutorial/example")
+        })
+public class ExampleMDB implements MessageListener {
+
+    public void onMessage(Message recvMsg) {
+      System.out.println("----------------");
+      System.out.println("Received message");
+      System.out.println("----------------");
+   }
+
+}
