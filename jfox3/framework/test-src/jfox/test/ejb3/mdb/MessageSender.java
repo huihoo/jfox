9e1fdf33af90cc47b9f02a142283fcfea27f279e
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-23 02:47:22 +0000

    Validation

---
+++ jfox3/framework/test-src/jfox/test/ejb3/mdb/MessageSender.java
@@ -0,0 +1,11 @@
+package jfox.test.ejb3.mdb;
+
+/**
+ * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
+ */
+public interface MessageSender {
+
+    void sendQuqueMessage();
+
+    void sendTopicMessage();
+}
---
+++ jfox3/framework/test-src/jfox/test/ejb3/mdb/MessageSenderBean.java
@@ -0,0 +1,37 @@
+package jfox.test.ejb3.mdb;
+
+import javax.ejb.Stateless;
+import javax.ejb.Remote;
+import javax.ejb.SessionContext;
+import javax.ejb.EJBException;
+import javax.annotation.Resource;
+import javax.jms.QueueConnectionFactory;
+
+import org.jfox.ejb3.naming.JNDIContextHelper;
+
+/**
+ * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
+ */
+@Stateless
+@Remote
+public class MessageSenderBean implements MessageSender {
+
+    @Resource
+    SessionContext sessionContext;
+
+    public void sendQuqueMessage() {
+        QueueConnectionFactory qcf = (QueueConnectionFactory)sessionContext.lookup("defaultcf");
+        //TODO: finish sendQuqueMessage
+
+    }
+
+    public void sendTopicMessage() {
+        try {
+            QueueConnectionFactory qcf = (QueueConnectionFactory)JNDIContextHelper.lookup("defaultcf");
+            //TODO: finish sendTopicMessage
+        }
+        catch (Exception e) {
+            throw new EJBException(e);
+        }
+    }
+}
