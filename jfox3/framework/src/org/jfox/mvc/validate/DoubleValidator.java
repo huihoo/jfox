0f159557f96058c917662e74c7b73fe838fbe4d6
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-23 02:38:11 +0000

    Validation

--- jfox3/framework/src/org/jfox/mvc/validate/DoubleValidator.java
+++ jfox3/framework/src/org/jfox/mvc/validate/DoubleValidator.java
@@ -5,11 +5,16 @@ import java.lang.annotation.Annotation;
 /**
  * @author <a href="mailto:jfox.young@gmail.com">Yang Yong</a>
  */
-public class DoubleValidator implements Validator<Double>{
+public class DoubleValidator implements Validator<Double> {
 
     public Double validate(String inputValue, Annotation validation) throws ValidateException {
-        DoubleValidation intv = (DoubleValidation) validation;
-        return Double.valueOf(inputValue);
+        DoubleValidation intv = (DoubleValidation)validation;
+        try {
+            return Double.valueOf(inputValue);
+        }
+        catch (NumberFormatException e) {
+            throw new ValidateException("Illegal double format for input: " + inputValue, inputValue);
+        }
     }
 
     public static void main(String[] args) {
--- jfox3/framework/src/org/jfox/mvc/validate/FloatValidator.java
+++ jfox3/framework/src/org/jfox/mvc/validate/FloatValidator.java
@@ -5,11 +5,17 @@ import java.lang.annotation.Annotation;
 /**
  * @author <a href="mailto:jfox.young@gmail.com">Yang Yong</a>
  */
-public class FloatValidator implements Validator<Float>{
+public class FloatValidator implements Validator<Float> {
 
     public Float validate(String inputValue, Annotation validation) throws ValidateException {
-        FloatValidation intv = (FloatValidation) validation;
-        return Float.valueOf(inputValue);
+        FloatValidation intv = (FloatValidation)validation;
+        try {
+            return Float.valueOf(inputValue);
+        }
+        catch (NumberFormatException e) {
+            throw new ValidateException("Illegal float format for input: " + inputValue, inputValue);
+        }
+
     }
 
     public static void main(String[] args) {
