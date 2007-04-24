5cf7241073928e6080f0f04005a37db8fe8e2181
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2007-04-24 08:42:01 +0000

    MDB

--- jfox3/framework/src/org/jfox/entity/dependent/FieldPersistenceUnitDependence.java
+++ jfox3/framework/src/org/jfox/entity/dependent/FieldPersistenceUnitDependence.java
@@ -1,44 +1,44 @@
 package org.jfox.entity.dependent;
 
 import java.lang.reflect.Field;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 
+import org.jfox.ejb3.EJBBucket;
+import org.jfox.entity.EntityManagerFactoryBuilderImpl;
 import org.jfox.framework.dependent.Dependence;
 import org.jfox.framework.dependent.InjectionException;
-import org.jfox.ejb3.EJBBucket;
-import org.jfox.entity.EntityManagerExt;
-import org.jfox.entity.EntityManagerFactoryBuilderImpl;
 
 /**
- * TODO: 注入 @PersistenceUnit
+ * 注入 @PersistenceUnit
  *
  * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
  */
-public class FieldPersistenceUnittDependence implements Dependence {
+public class FieldPersistenceUnitDependence implements Dependence {
 
     private EJBBucket bucket;
     private Field field;
-    private PersistenceContext pc;
+    private PersistenceUnit pu;
 
-    public FieldPersistenceUnittDependence(EJBBucket bucket, Field field, PersistenceContext pc) {
+    public FieldPersistenceUnitDependence(EJBBucket bucket, Field field, PersistenceUnit pu) {
         this.bucket = bucket;
         this.field = field;
-        this.pc = pc;
+        this.pu = pu;
     }
 
     public void inject(Object instance) throws InjectionException {
-        EntityManagerExt em;
-        String unitName = pc.unitName();
+        EntityManagerFactory emf;
+        String unitName = pu.unitName();
         if (unitName.trim().length() == 0) {
-            em = (EntityManagerExt)EntityManagerFactoryBuilderImpl.getDefaultEntityManagerFactory();
+            emf = EntityManagerFactoryBuilderImpl.getDefaultEntityManagerFactory();
         }
         else {
-            em = (EntityManagerExt)EntityManagerFactoryBuilderImpl.getEntityManagerFactoryByName(unitName).createEntityManager();
+            emf = EntityManagerFactoryBuilderImpl.getEntityManagerFactoryByName(unitName);
         }
         // 使用 field 反射注入
         try {
             field.setAccessible(true);
-            field.set(instance, em);
+            field.set(instance, emf);
         }
         catch (Exception e) {
             throw new InjectionException("Failed to inject field " + field.getName() + " of @PersistenceContext " + bucket.getEJBName(), e);
