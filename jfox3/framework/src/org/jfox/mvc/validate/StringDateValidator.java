22f9e9f073868977afededdd985cfb62a6284e59
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2008-01-10 07:41:12 +0000

    完善字典数据

---
+++ jfox3/framework/src/org/jfox/mvc/validate/StringDateValidator.java
@@ -0,0 +1,42 @@
+/*
+ * JFox - The most lightweight Java EE Application Server!
+ * more details please visit http://www.huihoo.org/jfox or http://www.jfox.org.cn.
+ *
+ * JFox is licenced and re-distributable under GNU LGPL.
+ */
+package org.jfox.mvc.validate;
+
+import java.lang.annotation.Annotation;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author <a href="mailto:jfox.young@gmail.com">Yang Yong</a>
+ */
+public class StringDateValidator implements Validator<String>{
+
+    public String validate(String inputValue, Annotation validation) throws ValidateException {
+        DateValidation dateValidation = (DateValidation)validation;
+        if (inputValue == null || inputValue.trim().length() == 0) {
+            if (!dateValidation.nullable()) {
+                throw new ValidateException("input can not be null!", inputValue);
+            }
+            else {
+                return "";
+            }
+        }
+        DateFormat dateFormat = new SimpleDateFormat(dateValidation.format());
+        try {
+            dateFormat.parse(inputValue);
+            return inputValue;
+        }
+        catch(ParseException e) {
+            throw new ValidateException("input date error, format: " + dateValidation.format(), e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
