b22fbc5cb18434bd6a3590d7cda17c22a6e85193
jfox.young <jfox.young@8d592abc-3327-0410-a519-f7f2e55ad719>
2008-07-06 15:05:41 +0000

    增加条件 Select

--- jfox3/framework/src/org/jfox/entity/dao/Condition.java
+++ jfox3/framework/src/org/jfox/entity/dao/Condition.java
@@ -18,7 +18,7 @@ public interface Condition {
         EQUAL("="),
         LESS("<"),
         LESS_EQUAL("<="),
-        IN("IN");
+        LIKE("LIKE");
 
         private String operator;
         OPERATOR(String operator) {
